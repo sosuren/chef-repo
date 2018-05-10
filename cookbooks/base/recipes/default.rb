03a5e52d80445cce778542b0f18ac374cd435676
Suren Yonjan <surendrayonjan@gmail.com>
2018-05-10 22:38:36 +0545

    setup docker in default recipe of base cookbook
    Requirements:
    VBoxManage 5.2.10r122088
    Vagrant 2.1.0
    Chef Development Kit Version: 2.5.3
    chef-client version: 13.8.5
    delivery version: master (73ebb72a6c42b3d2ff5370c476be800fee7e5427)
    berks version: 6.3.1
    kitchen version: 1.20.0
    inspec version: 1.51.21
    
    Commands:
    
    1. `kitchen create default-ubuntu-1604`
    2. `kitchen converge default-ubuntu-1604`
    3. `kitchen verify default-ubuntu-1604`
    4. `kitchen test default-ubuntu-1604`

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'chef-apt-docker'
+
+package 'docker-ce'
+
+docker_installation_package 'default' do
+  version '18.03.1'
+  action :create
+end
